fcac8f9a8d563d1ade25e3d471500bd0243f245f
Ayushi Mishra <mishrayu1998@gmail.com>
2026-03-15 22:28:33 +0530

    customHpook

---
+++ src/components/ToggleButtonWithCustomHook/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function useToggle(initialValue = false) {
+  const [value, setValue] = useState(initialValue);
+  const toggle = () => setValue((prev) => !prev);
+  return [value, toggle];
+}
+
+export default function App() {
+
+  const [isOn, toggle] = useToggle(false);
+
+  return (
+    /* your toggle function for the onClick method */
+    <button data-testid="toggle-button" onClick={toggle}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+}
