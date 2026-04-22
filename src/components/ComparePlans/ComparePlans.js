7ad1a94f4f1e66bcb8c5743bed35d21ab11b5cc5
Ayushi Mishra <mishrayu1998@gmail.com>
2026-04-22 13:10:11 +0530

    ComparePlans

--- src/components/ComparePlans/ComparePlans.js
+++ src/components/ComparePlans/ComparePlans.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ComparePlans.module.css";
 
 export default function ComparePlans() {
@@ -17,6 +17,9 @@ export default function ComparePlans() {
         C3: { title: "Theft & Fire Cover", value: true },
         C4: { title: "Zero Depreciation Cover", value: false },
         C5: { title: "Engine Secure", value: true },
+        C6: { title: "Theft & Fire Cover", value: true },
+        C7: { title: "Zero Depreciation Cover", value: false },
+        C8: { title: "Engine Secure", value: true },
       },
     },
     {
@@ -33,6 +36,9 @@ export default function ComparePlans() {
         C3: { title: "Theft & Fire Cover", value: true },
         C4: { title: "Zero Depreciation Cover", value: true },
         C5: { title: "Engine Secure", value: true },
+        C6: { title: "Theft & Fire Cover", value: true },
+        C7: { title: "Zero Depreciation Cover", value: false },
+        C8: { title: "Engine Secure", value: true },
       },
     },
     {
@@ -49,87 +55,127 @@ export default function ComparePlans() {
         C3: { title: "Theft & Fire Cover", value: true },
         C4: { title: "Zero Depreciation Cover", value: false },
         C5: { title: "Engine Secure", value: false },
+        C6: { title: "Theft & Fire Cover", value: true },
+        C7: { title: "Zero Depreciation Cover", value: false },
+        C8: { title: "Engine Secure", value: true },
       },
     },
   ];
-
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className={styles["CmprPlns"]}>
-      <div className={styles["headerRow"]}>
-        <h1 className={styles["cmprPlnHdr"]}>Compare Plans</h1>
-        <div className={styles["crossStack"]}>
-          <img
-            className={styles["crossImage"]}
-            src="/images/cross.svg"
-            alt="cross background"
-          />
-          <img
-            className={styles["crossImg"]}
-            src="/images/crossIcon.svg"
-            alt="cross icon"
-          />
-        </div>
-      </div>
-      <div className={styles["planTypeContainers"]}>
-        {plans.map((plan, id) => {
-          return (
-            <div key={id} className={styles["planName"]}>
+    <>
+      {toggle ? (
+        <div className={styles["CmprPlns"]}>
+          <div className={styles["headerRow"]}>
+            <h1 className={styles["cmprPlnHdr"]}>Compare Plans</h1>
+            <div className={styles["crossStack"]}>
               <img
-                className={styles["planImage"]}
-                src="/images/planName.svg"
-                alt={`${plan.name} badge`}
+                className={styles["crossImage"]}
+                src="/images/cross.svg"
+                alt="cross background"
               />
-              <div className={styles["planContent"]}>
-                <h2 style={{ color: "#264A9F" }}>{plan.name}</h2>
-                <p
-                  style={{
-                    color: "black",
-                    fontSize: "14px",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                  }}
-                >
-                  {plan.price}
-                </p>
-              </div>
+              <img
+                className={styles["crossImg"]}
+                src="/images/crossIcon.svg"
+                alt="cross icon"
+                onClick={() => setToggle((prev) => !prev)}
+              />
             </div>
-          );
-        })}
-      </div>
-      <div>
-        {Object.keys(plans[0].covers).map((coverKey, id) => {
-          return (
-            <div key={id} className={styles["coverRow"]}>
-              <div className={styles["coverTitleContainer"]}>
-                <p className={styles["coverTitle"]}>
-                  {plans[0].covers[coverKey].title}
-                </p>
-                <img src="./images/toolTipBackground.svg" alt="tool_tip_backkground" />
-              </div>
+          </div>
 
-              <div className={styles["coverValues"]}>
-                {plans.map((plan, id) => {
-                  return (
-                     <div key={id} className={styles["coverImageWrapper"]}>
-                      <img className={styles["coverImage"]}
-                      key={id}
-                      src={
-                        plan.covers[coverKey].value
-                          ? "/images/tick.svg"
-                          : "/images/false.svg"
-                      }
-                      alt={`${plan.name} ${coverKey} cover`}
-                    />
+          <div className={styles["planTypeContainers"]}>
+            {plans.map((plan, id) => {
+              return (
+                <div key={id} className={styles["planName"]}>
+                  <img
+                    className={styles["planImage"]}
+                    src="/images/planName.svg"
+                    alt={`${plan.name} badge`}
+                  />
+                  <div className={styles["planContent"]}>
+                    <h2 style={{ color: "#264A9F" }}>{plan.name}</h2>
+                    <p
+                      style={{
+                        color: "black",
+                        fontSize: "14px",
+                        fontStyle: "normal",
+                        fontWeight: 400,
+                      }}
+                    >
+                      {plan.price}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {Object.keys(plans[0].covers).map((coverKey, id) => {
+              return (
+                <div key={id} className={styles["coverRow"]}>
+                  <div className={styles["coverTitleContainer"]}>
+                    <p className={styles["coverTitle"]}>
+                      {plans[0].covers[coverKey].title}
+                    </p>
+                    <span className={styles["tooltipWrapper"]}>
+                      <img
+                        src="./images/toolTipBackground.svg"
+                        alt="tool_tip_backkground"
+                      />
+                      <span className={styles["tooltip"]}>
+                        {plans[0].description}
+                      </span>
+                    </span>
+                  </div>
 
-                      </div>
-                    
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+                  <div className={styles["coverValues"]}>
+                    {plans.map((plan, id) => {
+                      return (
+                        <div key={id} className={styles["coverImageWrapper"]}>
+                          <img
+                            className={styles["coverImage"]}
+                            key={id}
+                            src={
+                              plan.covers[coverKey].value
+                                ? "/images/tick.svg"
+                                : "/images/false.svg"
+                            }
+                            alt={`${plan.name} ${coverKey} cover`}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <button
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            alignSelf: "center",
+            display: "flex",
+            margin: "auto",
+            padding: "10px 20px",
+            backgroundColor: "#264A9F",
+            color: "white",
+            fontSize: "16px",
+            fontStyle: "normal",
+            fontWeight: 600,
+            borderRadius: "4px",
+            border: "none",
+            cursor: "pointer",
+          }}
+          onClick={() => setToggle((prev) => !prev)}
+        >
+          Get detailed plan comparsion
+        </button>
+      )}
+    </>
   );
 }
